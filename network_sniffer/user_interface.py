400e64caebe58001c8226ec6b9688bf602539dd1
Peter Lenon <goshomip@gmail.com>
2024-07-21 14:04:20 -0700

    added network sniffer challenge

---
+++ network_sniffer/user_interface.py
@@ -0,0 +1,117 @@
+import pygame
+from loguru import logger
+from packet import get_packets
+
+class NetworkSniffer:
+    def __init__(self):
+        self.id = "network_sniffer"
+        pygame.init()
+        self.tab = ' ' * 5
+        self.screen_width = 1000
+        self.screen_height = 800
+        self.WHITE = (255, 255, 255)
+        self.BLACK = (0, 0, 0)
+        self.GRAY = (200, 200, 200)
+        self.screen = self.initialize_screen()
+        self.font = self.set_font(None, 20)
+        self.instruction_text = self.get_challenge_instructions()
+        self.editor_rect = self.render_editor_surface()
+        self.running = True
+        self.users_code = "def network_sniffer(packets): \n" + self.tab
+        self.run_button = self.render_run_button()
+        self.packets = get_packets(10)
+        self.packet_ids = self.__get_packet_ids()
+
+    def initialize_screen(self):
+        screen = pygame.display.set_mode((self.screen_width, self.screen_height))
+        pygame.display.set_caption('Network Sniffer')
+        return screen
+
+    def set_font(self, fontName, fontsize):
+        return pygame.font.Font(fontName, fontsize)
+
+    def get_challenge_instructions(self):
+        instruction_text = [
+            "Instructions:",
+            "1. Our network traffic has been compromised and we need you to help us \n retrieve all compromised packets by packet id.",
+            "2. The packet class looks like this:\n"
+            "{\n"
+                f"{self.tab}id: SOME_ID,\n"
+                f"{self.tab}Raw:" + " {\n"
+                    f"{self.tab}{self.tab}user_id: userpassword\n"
+                f"{self.tab}" +"}\n"
+            "}\n"
+            "3. The packet class only has 2 methods:\n"
+                               f"{self.tab}.has_layer() ->" + "(Raw == {})\n"
+                                f"{self.tab}.load() --> Raw_object()\n"
+            "4.Click run when you are done to test your changes"
+        ]
+        return instruction_text
+
+    def render_editor_surface(self):
+        editor_rect = pygame.Rect(self.screen_width // 2, 0, self.screen_width // 2, self.screen_height)
+        return editor_rect
+
+    def render_text_to_surface(self, surface, text, position, font, color):
+        lines = text.split('\n')
+        y_offset = 0
+        for line in lines:
+            text_surface = font.render(line, True, color)
+            surface.blit(text_surface, (position[0], position[1] + y_offset))
+            y_offset += text_surface.get_height() + 5
+
+    def validate_user_code(self, user_input):
+        try:
+            local_vars = {}
+            exec(user_input, globals(), local_vars)
+            return self.packet_ids == local_vars['network_sniffer'](self.packets)
+        except Exception as e:
+            return False
+
+    def __get_packet_ids(self):
+        return [packet.id for packet in self.packets]
+
+    def is_run_code_button_pressed(self, run_button):
+        mouse_pos = pygame.mouse.get_pos()
+        mouse_pressed = pygame.mouse.get_pressed()
+        if mouse_pressed[0]:
+            if run_button.collidepoint(mouse_pos):
+                return True
+        return False
+
+    def render_run_button(self):
+        button = pygame.Rect(30, self.screen_height - 60, 75, 30)
+        pygame.draw.rect(self.screen, self.BLACK, button)
+        self.render_text_to_surface(self.screen, "RUN", [button.x + 25, button.y + 10], self.font, self.GRAY)
+        return button
+
+    def run(self):
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+                elif self.is_run_code_button_pressed(self.run_button):
+                    if self.validate_user_code(self.users_code):
+                        logger.info(f"Passed!")
+                        self.running = False
+                    else:
+                        logger.info(f"Wrong code!")
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RETURN:
+                        self.users_code += '\n' + self.tab
+                    elif event.key == pygame.K_TAB:
+                        self.users_code += self.tab
+                    elif event.key == pygame.K_BACKSPACE:
+                        self.users_code = self.users_code[:-1]
+                    else:
+                        self.users_code += event.unicode
+            self.screen.fill(self.WHITE)
+            self.render_text_to_surface(self.screen, '\n'.join(self.instruction_text), (20, 20), self.font, self.BLACK)
+            self.render_run_button()
+            pygame.draw.rect(self.screen, self.GRAY, self.editor_rect)
+            self.render_text_to_surface(self.screen, self.users_code, (self.editor_rect.x + 10, 20), self.font, self.BLACK)
+            pygame.display.flip()
+
+if __name__ == '__main__':
+    challenge = NetworkSniffer()
+    challenge.run()
