74cb03702604d1eacf37aa96c78cbd04a5914b0b
mahdishahsavar <mshahsavar@unr.edu>
2024-07-23 18:10:25 -0700

    Made classes for objects, add menu

--- utils/node_class.py
+++ utils/node_class.py
@@ -1,4 +1,16 @@
+import pygame
 class Node:
-    def __init__(self, id, position):
-        self.node_id = id
+    def __init__(self, id, position, size, color):
+        self.id = id
         self.position = position
+        self.size = size
+        self.color = color
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, self.color, self.position, self.size)
+
+    def detect_collision(self, player_pos, player_size):
+        p_x, p_y = player_pos
+        n_x, n_y = self.position
+        distance = ((p_x - n_x) ** 2 + (p_y - n_y) ** 2) ** 0.5
+        return distance < player_size / 2 + self.size
---
+++ utils/player_class.py
@@ -0,0 +1,50 @@
+import pygame
+
+class Player:
+    def __init__(self, position, color, size, speed):
+        self.position = list(position)  # Convert tuple to list if needed for mutability
+        self.color = color
+        self.size = size
+        self.speed = speed
+        self.path_orientation = 'horizontal'  # Initial orientation
+
+    def move(self, keys, paths):
+        # Handle movement, check path constraints
+        if keys[pygame.K_LEFT]:
+            new_pos = (self.position[0] - self.speed, self.position[1])
+            if self.is_on_path(self.position, new_pos, paths):
+                self.position[0] -= self.speed
+                self.path_orientation = 'horizontal'
+        if keys[pygame.K_RIGHT]:
+            new_pos = (self.position[0] + self.speed, self.position[1])
+            if self.is_on_path(self.position, new_pos, paths):
+                self.position[0] += self.speed
+                self.path_orientation = 'horizontal'
+        if keys[pygame.K_UP]:
+            new_pos = (self.position[0], self.position[1] - self.speed)
+            if self.is_on_path(self.position, new_pos, paths):
+                self.position[1] -= self.speed
+                self.path_orientation = 'vertical'
+        if keys[pygame.K_DOWN]:
+            new_pos = (self.position[0], self.position[1] + self.speed)
+            if self.is_on_path(self.position, new_pos, paths):
+                self.position[1] += self.speed
+                self.path_orientation = 'vertical'
+
+    def draw(self, screen):
+        # Draw the player on the screen based on the current orientation
+        if self.path_orientation == 'horizontal':
+            adjusted_position = (self.position[0], self.position[1] - self.size // 2)
+        elif self.path_orientation == 'vertical':
+            adjusted_position = (self.position[0] - self.size // 2, self.position[1])
+        pygame.draw.rect(screen, self.color, (*adjusted_position, self.size, self.size))
+
+    def is_on_path(self, old_pos, new_pos, paths):
+        for path in paths:
+            if path.start[0] == path.end[0]:  # Vertical path
+                if path.start[0] == new_pos[0] and path.start[1] <= new_pos[1] <= path.end[1]:
+                    return True
+            elif path.start[1] == path.end[1]:  # Horizontal path
+                if path.start[1] == new_pos[1] and path.start[0] <= new_pos[0] <= path.end[0]:
+                    return True
+        return False
